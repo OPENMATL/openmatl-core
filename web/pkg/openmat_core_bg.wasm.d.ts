4cc7d673d5267befd62f56818fbc3fdd12683b00
ABHIJAT SARARI <251164443+Bosonbrain@users.noreply.github.com>
2026-08-25 17:49:18 +0530

    Fixed missing WebAssembly binaries in web directory

---
+++ web/pkg/openmat_core_bg.wasm.d.ts
@@ -0,0 +1,12 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const __wbg_wasmengine_free: (a: number, b: number) => void;
+export const wasmengine_eval: (a: number, b: number, c: number) => [number, number, number];
+export const wasmengine_new: () => number;
+export const wasmengine_write_file: (a: number, b: number, c: number, d: number, e: number) => void;
+export const __wbindgen_externrefs: WebAssembly.Table;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __externref_table_dealloc: (a: number) => void;
+export const __wbindgen_start: () => void;
